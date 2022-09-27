9edeadd9233692a27130fc905b1c15554bbc8419
Imran940 <immoshaikh91@gmail.com>
2022-09-27 23:23:54 +0530

    added files

---
+++ index.js
@@ -0,0 +1,20 @@
+// const Scraper = require("website-scraper");
+import Scraper from "website-scraper"
+console.log("Hey harshad let's learn nodejs basics");
+
+let options = {
+  urls: ["https://github.com/"],
+  directory: "./dowloadedSourceCode",
+};
+
+// function demo(a){
+// console.log("demo is called");
+// }
+
+// demo(5)
+Scraper(options)
+  .then((data) => {
+    console.log(data);
+    console.log("source code of website downloaded.. ");
+  })
+  .catch((err) => console.log(err));
